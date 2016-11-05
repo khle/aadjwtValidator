1830a08ccb305f11c93c8b8d5291ed460fe55806
Kevin Le <pragmaticlogic@gmail.com>
2016-11-05 00:43:32 +0000

    https

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var path = require('path'),
     express = require('express'),
     bodyParser = require('body-parser'),
+    https = require('https'),
+    fs = require('fs'),
     jwtValidator = require('./jwtValidator');
+
+var options = {
+    key:    fs.readFileSync('certs/aadjwt_codeprototype_com.key'),
+    cert:   fs.readFileSync('certs/aadjwt_codeprototype_com.crt'),
+    ca:     fs.readFileSync('certs/ca_bundle.crt'),
+    requestCert:        true,
+    rejectUnauthorized: false
+};
 
 var app = express();
 
@@ -25,6 +35,7 @@ app.use(bodyParser.urlencoded({
         extended: true
     }));
 app.use('/', express.static(path.join(__dirname, 'app')));
+//app.use('/.well-known/acme-challenge', express.static(__dirname + '/.well-known/acme-challenge'));
 app.use('/validate/', function (req, res) {
   var result = validate(req.body.jwt, function(result) {
     console.log(result);
@@ -32,6 +43,6 @@ app.use('/validate/', function (req, res) {
   });
 });
 
-var server = app.listen(5003, function () {
+var server = https.createServer(options, app).listen(5003, function () {
     console.log('Server listening on port ', server.address().port);
 });
